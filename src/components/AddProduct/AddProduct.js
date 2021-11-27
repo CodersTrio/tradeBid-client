da020fef616a811e584a7fdcbf2de1aabb83a48a
Ashraful Islam <ashrafunit7@gmail.com>
2021-11-28 01:23:50 +0600

    add product is functional and showing to homepage ui

--- src/components/AddProduct/AddProduct.js
+++ src/components/AddProduct/AddProduct.js
@@ -1,49 +1,114 @@
-import React from 'react';
-import { Form, Button, FloatingLabel } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Form, Button, FloatingLabel, Card } from 'react-bootstrap';
+import axios from "axios";
+
 
 const AddProduct = () => {
-    return (
-       <div className="w-75 mx-auto mt-3">
-           <h1 className="text-center">Add Product</h1>
-            <Form>
-            <Form.Group className="mb-3">
-                <Form.Label>Product Category</Form.Label>
-                <Form.Control type="text" placeholder="Product Category" />
-            </Form.Group>
+  const [product, setProduct] = useState({});
+  const [imageURL, setImageURL] = useState(null);
 
-            <Form.Group className="mb-3">
-                <Form.Label>Product Name</Form.Label>
-                <Form.Control type="text" placeholder="Product Name" />
-            </Form.Group>
+  const handleBlur = (e) => {
+    const newData = { ...product };
+    newData[e.target.name] = e.target.value;
+    setProduct(newData);
+    console.log(newData);
+  };
 
-            <Form.Group className="mb-3">
-                <Form.Label>Lowest bid price</Form.Label>
-                <Form.Control type="digit" placeholder="Lowest bid" />
-            </Form.Group>
+  const handleSubmit = (e) => {
+    const formData = {
+      category: product.category,
+      name: product.name,
+      lowestBid: product.lowestBid,
+      expired: product.expired,
+      details: product.details,
+      imageURL: imageURL,
+    };
 
-            <Form.Group className="mb-3">
-                <Form.Label>Bid expiry date</Form.Label>
-                <Form.Control type="date" placeholder="Bid expired" />
-            </Form.Group>
+    fetch("https://serene-savannah-96392.herokuapp.com/addProduct", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(formData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data) {
+          alert("Product Added successfully!");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
 
-            <FloatingLabel controlId="floatingTextarea2" label="Products details">
-                <Form.Control
-                    as="textarea"
-                    placeholder="Leave a comment here"
-                    style={{ height: '100px' }}
-                />
-            </FloatingLabel>
+    e.preventDefault();
+  };
 
-            <br />
+  const handleImageUpload = (event) => {
+    console.log(event.target.files[0]);
+    const imageData = new FormData();
+    imageData.set("key", "b86c0ab7beeb42c384775d3b62a113c0");
+    imageData.append("image", event.target.files[0]);
 
-            <div className="text-center">
-                <Button className="px-5 py-2" variant="primary" type="submit">
-                    Submit
-                </Button>
-            </div>
-        </Form>
-       </div>
-    );
+    axios
+      .post("https://api.imgbb.com/1/upload", imageData)
+      .then(function (response) {
+        setImageURL(response.data.data.display_url);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="w-75 mx-auto mt-3">
+      <h1 className="text-center">Add Product</h1>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3">
+          <Form.Label>Product Category</Form.Label>
+          <Form.Control type="text" placeholder="Product Category" name="category" onBlur={handleBlur} />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Product Name</Form.Label>
+          <Form.Control type="text" placeholder="Product Name" name="name" onBlur={handleBlur} />
+        </Form.Group>
+
+        <Form.Group controlId="formFile" className="mb-3">
+          <Form.Label>Choose an image file</Form.Label>
+          <Form.Control type="file" name="image"
+            onChange={handleImageUpload} />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Lowest bid price</Form.Label>
+          <Form.Control type="digit" placeholder="Lowest bid" name="lowestBid" onBlur={handleBlur} />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Bid expiry date</Form.Label>
+          <Form.Control type="date" placeholder="Bid expired" name="expired" onBlur={handleBlur} />
+        </Form.Group>
+
+        <FloatingLabel controlId="floatingTextarea2" label="Products details">
+          <Form.Control
+            as="textarea"
+            placeholder="Leave a comment here"
+            style={{ height: '100px' }}
+            name="details"
+            onBlur={handleBlur}
+          />
+        </FloatingLabel>
+
+        <br />
+
+        <div className="text-center">
+          <Button className="px-5 py-2" variant="primary" type="submit">
+            Submit
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
 };
 
 export default AddProduct;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import SignIn from '../../components/authentication/SignIn/SignIn'; 
+import React, { useEffect, useState } from 'react';
 import Header from '../../components/Header/Header';
+import ProductCard from '../../components/ProductCard/ProductCard';
+import './Home.css';
 
 const Home = () => {
+    const [productsData, setProductsData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://serene-savannah-96392.herokuapp.com/products")
+      .then((res) => res.json())
+      .then((data) => {
+        setProductsData(data);
+      });
+  }, []);
+  console.log(productsData)
     return (
         <div>
             <Header/>
-            {/* <SignIn /> */}
             <h1>Hello Home</h1>
+            <div className="productsCard d-flex flex-wrap mx-auto text-center justify-content-around">
+            {
+                productsData.map((product) => <ProductCard product={product} key={product._id} />)
+            }
+            </div>
         </div>
     );
 };
