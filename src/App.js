b08ed31087df6e4722184ca3f7ec3341923a7d83
Ashraful Islam <ashrafunit7@gmail.com>
2021-11-27 16:20:21 +0600

    addProduct component added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import AddProduct from './components/AddProduct/AddProduct';
+import SignIn from './components/authentication/SignIn/SignIn';
 import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div>
-      <Home /> 
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/signIn">
+            <SignIn></SignIn>
+          </Route>
+          <Route path="/addProduct">
+            <AddProduct></AddProduct>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
-  );  
+  );
 }
 
 export default App;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import SignIn from '../../components/authentication/SignIn/SignIn'; 
+import Header from '../../components/Header/Header';
 
 const Home = () => {
     return (
         <div>
-            <SignIn />
+            <Header/>
+            {/* <SignIn /> */}
         </div>
     );
 };
