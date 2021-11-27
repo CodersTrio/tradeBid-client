b62a0c5cd17d1f60ae74425e9e6612eb92fd1dda
Ashraful Islam <ashrafunit7@gmail.com>
2021-11-27 11:36:49 +0600

    env setup

---
+++ src/components/Firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyApxmS2fuPFQp7OvqXB_GAYpHEkYRyavEM",
+    authDomain: "tradebid-ee999.firebaseapp.com",
+    projectId: "tradebid-ee999",
+    storageBucket: "tradebid-ee999.appspot.com",
+    messagingSenderId: "612463272072",
+    appId: "1:612463272072:web:866b12288a25306730bf3d"
+  };
+
+  export default firebaseConfig;
