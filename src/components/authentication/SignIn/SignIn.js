db855460c1e4e6e40b2424eb1e142354cc1736f8
Ashraful Islam <ashrafunit7@gmail.com>
2021-11-27 15:22:18 +0600

    signIn component formed

--- src/components/authentication/SignIn/SignIn.js
+++ src/components/authentication/SignIn/SignIn.js
@@ -5,24 +5,26 @@ import './SignIn.css';
 const SignIn = () => {
     return (
         <div className="sinInComponent">
+            <div className="signInContainer">
             <div className="SignIn">
-                <div className="row ">
-                    <div className="col-md-8 text-center signInContainer" style={{border: '2px solid red'}}>
+            <div className="row signInRow" style={{height:'70vh'}}>
+                    <div className="col-md-8 text-center inputFields" style={{padding:'20px'}}>
                         <h1>Welcome</h1>
                         <p>Log in to you account here.</p>
-                        <div className="inputFields">
-                        <input className="emailInp" type="email" name="email" placeholder="Enter you email address"/>
+                        <div className="">
+                        <input className="emailInp" type="email" name="email" placeholder="Enter your email address"/>
                         <br /><br />
                         <input className="passwordInp" type="password" name="password" placeholder="Enter your password"/>
                         <br /><br />
                         <Button className="py-2  px-5" variant="info">Log in</Button> 
                         </div>
                     </div>
-                    <div className="col-md-4 signUpNow" style={{border: '2px solid blue'}}>
+                    <div className="col-md-4 signUpNow" style={{padding: '20px', backgroundColor:'cyan'}}>
                         <div><h6 >Don't have an account? </h6></div>
                         <div><p>Sign Up</p></div>
                     </div>
                 </div>
+            </div>
             </div>
         </div>
     );
